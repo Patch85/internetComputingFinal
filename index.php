8b36c45032fb78ec4a85c3bae1986e352fb1058f
Patch85 <dillon.a.john@gmail.com>
2019-12-01 19:39:23 -0500

    Updated All Nav Bars for Mike's Search
    Updated the nav bars for all pages to include Mike's categorical search dropdown. Also styled the select/dropdown to match the Bootstrap styles of the rest of the site

--- index.php
+++ index.php
@@ -102,13 +102,13 @@
                 <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"
                     name="SearchQuery">
                 
-                <select name="categoryInput">
+                <select class="form-control mr-sm-2" name="categoryInput">
                     <option value="productName">Product Name</option>
                     <option value="category">Category</option>
                     <option value="subcategory">subcategory</option>
                     <option value="manufacturer">Manufacturer</option>
                 </select>
-                
+
                 <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
             </form>
         </div>
--- products.php
+++ products.php
@@ -99,6 +99,14 @@
             <form class="form-inline my-2 my-lg-0" action="searchResults.php" method="post">
                 <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"
                     name="SearchQuery">
+                
+                <select class="form-control mr-sm-2" name="categoryInput">
+                    <option value="productName">Product Name</option>
+                    <option value="category">Category</option>
+                    <option value="subcategory">subcategory</option>
+                    <option value="manufacturer">Manufacturer</option>
+                </select>
+
                 <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
             </form>
         </div>
