257a0249799fd68ab0b687a1c4e949cf2abdbc11
Mike <serafinomichael@gmail.com>
2019-12-04 19:17:43 -0500

    Fixed database connection

--- CustomerReceipts.php
+++ CustomerReceipts.php
@@ -1,9 +1,20 @@
 <?php
-$dsn = 'mysql:host=localhost;dbname=music';
-$user = 'admin';
-$pass = 'pa55word';
-
-$db = new PDO($dsn, $user, $pass);
+    // Start a session
+    session_start();
+    // Set initial variables
+    $dsn = 'mysql:host=localhost;dbname=music';
+    // musicman has global privileges for the music database
+    // make sure to create and use a more limited user for customer access
+    $dbUsername = 'musicman';
+    $dbPassword = 'bQC_2AFWpq46M4N';
+    //  Attempt to connect to the database
+    try {
+        $db = new PDO($dsn, $dbUsername, $dbPassword);
+    } catch (PDOException $e) {
+        $error_message = $e->getMessage();
+        echo " <p>An error occurred while connecting to the database: $error_message</p>";
+        die();
+    }
 
 //session_start();
 //$_SESSION['acctNum'] = 1;
