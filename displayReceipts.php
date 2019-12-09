7911afa2c3420b5666b5f5df1ff8d1e27376a142
Mike <serafinomichael@gmail.com>
2019-12-09 16:09:04 -0500

    Forgot HTML tag

--- displayReceipts.php
+++ displayReceipts.php
@@ -53,7 +53,7 @@ $statementGetOrderNums->closeCursor();
  */
 
 ?>
-
+<html lang="en" dir="ltr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
